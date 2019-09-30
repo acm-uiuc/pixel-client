b708487b5f261da91fa37b660a8c01201af5dae7
Christian Sparks <csparks1919@hotmail.com>
2019-09-30 16:08:22 -0500

    Terminal is working, and refactored to remove webpack

--- server/app.py
+++ server/app.py
@@ -1,13 +1,32 @@
+"""A single common terminal for all websockets.
+"""
 import tornado.web
-import tornado.ioloop
+# This demo requires tornado_xstatic and XStatic-term.js
+import tornado_xstatic
 
-options = {
-    "path": "./dist", 
-    "default_filename": "../dist/index.html"
-}
+from terminado import TermSocket, SingleTermManager
+from common import run_and_show_browser, STATIC_DIR, TEMPLATE_DIR
 
-if __name__ == "__main__":
-    app = tornado.web.Application([tornado.web.url(r"/(.*)", tornado.web.StaticFileHandler, options)])
+class TerminalPageHandler(tornado.web.RequestHandler):
+    def get(self):
+        return self.render("index.html", static=self.static_url,
+                           xstatic=self.application.settings['xstatic_url'],
+                           ws_url_path="/websocket")
 
-    app.listen(3000)
-    tornado.ioloop.IOLoop.current().start()
+def main(argv):
+    term_manager = SingleTermManager(shell_command=['bash'])
+    handlers = [
+                (r"/websocket", TermSocket,
+                     {'term_manager': term_manager}),
+                (r"/", TerminalPageHandler),
+                (r"/xstatic/(.*)", tornado_xstatic.XStaticFileHandler,
+                     {'allowed_modules': ['termjs']})
+               ]
+    app = tornado.web.Application(handlers, static_path=STATIC_DIR,
+                      template_path=TEMPLATE_DIR,
+                      xstatic_url = tornado_xstatic.url_maker('/xstatic/'))
+    app.listen(3000, 'localhost')
+    run_and_show_browser("http://localhost:3000/", term_manager)
+
+if __name__ == '__main__':
+    main([])
---
+++ server/client/script.js
@@ -0,0 +1,11 @@
+/*monaco.editor.create(document.getElementById('editor'), {
+  value: [
+    'def main():',
+    '  print("hello world")',
+    '  ',
+    'main()'
+  ].join('\n'),
+  language: 'python'
+});*/
+
+
