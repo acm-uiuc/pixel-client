b765186797072e13adbb648bb4150da26e8a4506
Marcin <mpitera483@gmail.com>
2019-10-14 17:14:26 -0500

    numers -> numbers

--- app/client/js/editor.js
+++ app/client/js/editor.js
@@ -1,7 +1,7 @@
 var myCodeMirror = CodeMirror(document.body, {
   value: "# Here's some code\n\n\ndef add(a, b):\n    return a + b\n\nprint(add(4, 5)) # Output: \"9\"",
   mode: "python",
-  lineNumers: true
+  lineNumbers: true
 });
 // var myCodeMirror2 = CodeMirror.fromTextArea(, {
 //   value: "#This is python\nmyScript():\n\tprint \'test\'",
