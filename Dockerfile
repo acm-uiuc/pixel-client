2170175e1816762817d080887de9411f0fb39780
Christian Sparks <csparks1919@hotmail.com>
2019-10-18 20:35:09 -0500

    Added containerization for all users.

--- Dockerfile
+++ Dockerfile
@@ -13,8 +13,5 @@ RUN pip install --trusted-host pypi.python.org -r ./server/requirements.txt
 # Make port 80 available to the world outside this container
 EXPOSE 80
 
-# Define environment variable
-ENV NAME World
-
 # Run app.py when the container launches
 CMD ["python", "./server/app.py"]
--- server/handlers/protected_term_manager.py
+++ server/handlers/protected_term_manager.py
@@ -1,4 +1,5 @@
 import os
+import docker
 
 from terminado import UniqueTermManager
 from util.user_gen import randomString
@@ -6,12 +7,20 @@ from util.user_gen import randomString
 class ProtectedTermManager(UniqueTermManager):
     def __init__(self, **kwargs):
         self.max_terminals = 500
-        command = self.user_gen()
-        super(UniqueTermManager, self).__init__(shell_command=command, **kwargs)
+        super(UniqueTermManager, self).__init__(shell_command=["bash"], **kwargs)
         self.terminal = None
+    
+    def get_terminal(self, url_component=None):
+        if self.max_terminals and len(self.ptys_by_fd) >= self.max_terminals:
+            raise MaxTerminalsReached(self.max_terminals)
+        
+        self.shell_command = self.user_gen()
+        term = self.new_terminal()
+        self.start_reading(term)
+        return term
 
     def user_gen(self):
         userid = "User"+randomString(28)
-        os.system('useradd -m '+userid)
-
-        return ["su", "-l", userid]
+        docker.from_env().containers.run("user-container", detach=True, name=userid)
+        
+        return ["./lib/docker", "exec", "-it", userid, "bash"]
