39e3c3b1f2a0b5f4b0fafdec6ae9c826c56659e3
virnarula <virnarula@outlook.com>
2019-10-21 17:49:13 -0500

    Add split.js css and js implementation

--- client/js/main.js
+++ client/js/main.js
@@ -1,10 +1,25 @@
 Split(['#info', '#input'], {
-  sizes: [25,75],
+
+  elementStyle: (dimension, size, gutterSize) => ({
+    'flex-basis': `calc(${size}% - ${gutterSize}px)`,
+  }),
+  gutterStyle: (dimension, gutterSize) => ({
+    'flex-basis': `${gutterSize}px`,
+  }),
+
+  sizes: [25, 75],
   minSize: 200,
   direction: 'vertical',
 })
 
 Split(['#terminal', '#editor'], {
-  sizes: [50,50],
+  elementStyle: (dimension, size, gutterSize) => ({
+    'flex-basis': `calc(${size}% - ${gutterSize}px)`,
+  }),
+  gutterStyle: (dimension, gutterSize) => ({
+    'flex-basis': `${gutterSize}px`,
+  }),
+
+  sizes: [50, 50],
   minSize: 200,
 })
