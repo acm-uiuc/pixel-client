6441444a5c4c6ef3a65936310d05bb424c93c1a5
virnarula <virnarula@outlook.com>
2019-10-24 17:37:26 -0500

    Fix codemirror placement bug

--- client/js/editor.js
+++ client/js/editor.js
@@ -1,10 +1,12 @@
-var myCodeMirror = CodeMirror(document.body, {
+let elm = document.createElement('div');
+let myCodeMirror = CodeMirror(elm, {
   value: "# Here's some code\n\n\ndef add(a, b):\n    return a + b\n\nprint(add(4, 5)) # Output: \"9\"",
   mode: "python",
   lineNumbers: true
 });
 
-setTimeout(function() {fetch("./file", {
+setTimeout(function() {
+  fetch("./file", {
   method: "post",
   headers: {
     'Accept': 'application/json',
@@ -17,29 +19,7 @@ setTimeout(function() {fetch("./file", {
     contents: "asdf"
   })
 })
-.then( (response) => { 
-  console.log(response)
-});}, 2000)
-// var myCodeMirror2 = CodeMirror.fromTextArea(, {
-//   value: "#This is python\nmyScript():\n\tprint \'test\'",
-//   mode: "python"
-// });
-
-
-
-fetch("./file", {
-  method: "post",
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-  },
-
-  //make sure to serialize your JSON body
-  body: JSON.stringify({
-    name: "test.txt",
-    contents: "asdf"
-  })
-})
-.then( (response) => { 
+.then( (response) => {
   console.log(response)
 });
+}, 3000)
--- client/js/main.js
+++ client/js/main.js
@@ -1,25 +1,26 @@
-Split(['#info', '#input'], {
-
-  elementStyle: (dimension, size, gutterSize) => ({
-    'flex-basis': `calc(${size}% - ${gutterSize}px)`,
-  }),
-  gutterStyle: (dimension, gutterSize) => ({
-    'flex-basis': `${gutterSize}px`,
-  }),
-
-  sizes: [25, 75],
-  minSize: 200,
-  direction: 'vertical',
-})
-
-Split(['#terminal', '#editor'], {
-  elementStyle: (dimension, size, gutterSize) => ({
-    'flex-basis': `calc(${size}% - ${gutterSize}px)`,
-  }),
-  gutterStyle: (dimension, gutterSize) => ({
-    'flex-basis': `${gutterSize}px`,
-  }),
-
-  sizes: [50, 50],
-  minSize: 200,
-})
+//
+// Split(['#info', '#input'], {
+//
+//   elementStyle: (dimension, size, gutterSize) => ({
+//     'flex-basis': `calc(${size}% - ${gutterSize}px)`,
+//   }),
+//   gutterStyle: (dimension, gutterSize) => ({
+//     'flex-basis': `${gutterSize}px`,
+//   }),
+//
+//   sizes: [25, 75],
+//   minSize: 200,
+//   direction: 'vertical',
+// })
+//
+// Split(['#terminal', '#editor'], {
+//   elementStyle: (dimension, size, gutterSize) => ({
+//     'flex-basis': `calc(${size}% - ${gutterSize}px)`,
+//   }),
+//   gutterStyle: (dimension, gutterSize) => ({
+//     'flex-basis': `${gutterSize}px`,
+//   }),
+//
+//   sizes: [50, 50],
+//   minSize: 200,
+// })
